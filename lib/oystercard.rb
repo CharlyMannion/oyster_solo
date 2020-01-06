16ad417c0a203a2b74eaa4ec6aa2766456a5fbe9
Charlotte Mannion <charlottemannion100@gmail.com>
2020-01-06 12:25:23 +0000

    initializes oysrcard with a default balance constant

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,3 +1,9 @@
 class Oystercard
+  DEFAULT_BALANCE = 10
+
   attr_accessor :balance
+
+  def initialize
+    @balance = DEFAULT_BALANCE
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,9 +1,17 @@
 require 'oystercard'
 
 describe Oystercard do
+  DEFAULT_BALANCE = 10
+
   subject(:oystercard) { described_class.new }
 
   it 'should respond to balance' do
     expect(oystercard).to respond_to :balance
+  end
+
+  describe '#initalize' do
+    it 'should have a default amount as starting balance' do
+      expect(oystercard.balance).to eq(DEFAULT_BALANCE)
+    end
   end
 end
