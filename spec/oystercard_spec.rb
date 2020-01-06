8c5100d5e30fb5c529af560e9d98396d4f92431d
Charlotte Mannion <charlottemannion100@gmail.com>
2020-01-06 12:29:07 +0000

    adds top_up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,6 +4,7 @@ describe Oystercard do
   DEFAULT_BALANCE = 10
 
   subject(:oystercard) { described_class.new }
+  top_up_amount = 10
 
   it 'should respond to balance' do
     expect(oystercard).to respond_to :balance
@@ -12,6 +13,14 @@ describe Oystercard do
   describe '#initalize' do
     it 'should have a default amount as starting balance' do
       expect(oystercard.balance).to eq(DEFAULT_BALANCE)
+    end
+  end
+
+  describe '#top_up' do
+    it 'should increase the oystercard by a specified amount' do
+      new_balance = DEFAULT_BALANCE + top_up_amount
+      oystercard.top_up(top_up_amount)
+      expect(oystercard.balance).to eq(new_balance)
     end
   end
 end
