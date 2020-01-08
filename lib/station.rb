6f35ef0c335baddee491e3070f4d2aabefaad6fe
Charlotte Mannion <charlottemannion100@gmail.com>
2020-01-08 10:56:38 +0000

    adding a barrier creates an instance of barrier and adds it to the station

--- lib/station.rb
+++ lib/station.rb
@@ -1,7 +1,15 @@
-class Station
-  attr_accessor :zone
+require_relative 'barrier'
 
-  def initialize
+class Station
+  attr_accessor :zone, :barriers
+
+  def initialize(barrier_class = Barrier)
     @zone
+    @barrier_class = barrier_class
+    @barriers = []
+  end
+
+  def add_barrier
+    @barriers << @barrier_class.new
   end
 end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -7,11 +7,21 @@ describe Station do
     it 'should have a zone' do
       expect(subject.zone).to eq(nil)
     end
+    it 'should have no barriers' do
+      expect(subject.barriers).to eq([])
+    end
 
     describe '#zone' do
       it 'should be possible to change the zone' do
         subject.zone = 1
         expect(subject.zone).to eq(1)
+      end
+    end
+
+    describe '#add_barrier' do
+      it 'should be possible to add barriers to the station' do
+        subject.add_barrier
+        expect(subject.barriers.first).to be_a Barrier
       end
     end
   end
