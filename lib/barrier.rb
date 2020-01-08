e24cdc724eb4d5ccd8fc475ac1cd1f11e7d9ea8b
Charlotte Mannion <charlottemannion100@gmail.com>
2020-01-08 15:22:29 +0000

    assigns a barrier to a station

--- lib/barrier.rb
+++ lib/barrier.rb
@@ -1,3 +1,12 @@
 class Barrier
-attr_accessor :station
+  attr_accessor :station
+
+  def initalize
+    @station
+  end
+
+  def assign_to(station)
+    @station = station
+  end
+
 end
--- spec/barrier_spec.rb
+++ spec/barrier_spec.rb
@@ -2,9 +2,17 @@ require 'barrier'
 
 describe Barrier do
   subject(:barrier) { described_class.new }
+  let(:station_double) { double :station, zone: 1 }
 
   it 'should respond to station' do
     expect(barrier).to respond_to(:station)
+  end
+
+  describe '#assign_to' do
+    it 'should assign a barrier to an instance of station' do
+      barrier.assign_to(station_double)
+      expect(barrier.station).to eq(station_double)
+    end
   end
 
 end
