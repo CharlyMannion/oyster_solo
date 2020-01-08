9894f0ab4ef625cf74cc9a7f79cca15bed7a6145
Charlotte Mannion <charlottemannion100@gmail.com>
2020-01-08 11:00:48 +0000

    minor refactor

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -2,6 +2,7 @@ require 'station'
 
 describe Station do
   subject(:station) { described_class.new }
+  let(:barrier_class_double) { double :barrier_class }
 
   describe 'initialize' do
     it 'should have a zone' do
@@ -10,19 +11,19 @@ describe Station do
     it 'should have no barriers' do
       expect(subject.barriers).to eq([])
     end
+  end
 
-    describe '#zone' do
-      it 'should be possible to change the zone' do
-        subject.zone = 1
-        expect(subject.zone).to eq(1)
-      end
+  describe '#zone' do
+    it 'should be possible to change the zone' do
+      subject.zone = 1
+      expect(subject.zone).to eq(1)
     end
+  end
 
-    describe '#add_barrier' do
-      it 'should be possible to add barriers to the station' do
-        subject.add_barrier
-        expect(subject.barriers.first).to be_a Barrier
-      end
+  describe '#add_barrier' do
+    it 'should be possible to add barriers to the station' do
+      subject.add_barrier
+      expect(subject.barriers.first).to be_a Barrier
     end
   end
 end
