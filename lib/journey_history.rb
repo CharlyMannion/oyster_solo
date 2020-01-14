ecfc87010b21fa84a93776b91526ebdfd63a0b3a
Charlotte Mannion <charlottemannion100@gmail.com>
2020-01-14 12:43:45 +0000

    iterating through journeys, but returning the entire object and not the string

--- lib/journey_history.rb
+++ lib/journey_history.rb
@@ -11,6 +11,9 @@ class JourneyHistory
 
   def display
     # test = "Journey History: #{jhist.journeys[0].entry_barrier.station.name} - #{jhist.journeys[0].exit_barrier.station.name}; #{jhist.journeys[1].entry_barrier.station.name} - #{jhist.journeys[1].exit_barrier.station.name}"
-    "Journey History: #{@journeys[0].entry_barrier.station.name} - #{@journeys[0].exit_barrier.station.name}; #{@journeys[1].entry_barrier.station.name} - #{@journeys[1].exit_barrier.station.name}"
+    # "Journey History: #{@journeys[0].entry_barrier.station.name} - #{@journeys[0].exit_barrier.station.name}; #{@journeys[1].entry_barrier.station.name} - #{@journeys[1].exit_barrier.station.name}"
+    @journeys.each { |journey|
+      p "#{journey.entry_barrier.station.name} - #{journey.exit_barrier.station.name};"
+     }
   end
 end
