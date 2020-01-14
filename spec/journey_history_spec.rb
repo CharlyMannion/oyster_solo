9e18ada911144ee2113724fd0ac1abc5798ab9fc
Charlotte Mannion <charlottemannion100@gmail.com>
2020-01-14 12:10:31 +0000

    returns to test in journey history, ammends test doubles

--- spec/journey_history_spec.rb
+++ spec/journey_history_spec.rb
@@ -7,7 +7,7 @@ describe JourneyHistory do
     let(:journey) { double :journey }
     let(:barrier) { double :barrier, station: station_double }
     let(:barrier_two) { double :barrier, station: station_double }
-    let(:station_double) { double :station, zone: 1 }
+    let(:station_double) { double :station, zone: 1, name: :dalston }
     let(:journey_double) { double :journey, entry_barrier: :barrier, exit_barrier: :barrier_two, complete: true }
     let(:journey_double_two) { double :journey, entry_barrier: :barrier_two, exit_barrier: :barrier, complete: true }
 
@@ -20,12 +20,12 @@ describe JourneyHistory do
       expect(jhist.journeys).to include(journey)
     end
 
-    # it 'should have a function to display the journey history to user' do
-    #   jhist.record(journey_double_two)
-    #   jhist.record(journey_double)
-    #   p "accessing the station below"
-    #   p journey_double.entry_barrier
-    #   expect(jhist.display).to eq('test')
-    # end
+    it 'should have a function to display the journey history to user' do
+      jhist.record(journey_double_two)
+      jhist.record(journey_double)
+      p "accessing the station below"
+      p journey_double.entry_barrier.name
+      expect(jhist.display).to eq('test')
+    end
   end
 end
